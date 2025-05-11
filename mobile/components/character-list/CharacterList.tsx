20c5a689d80aba8182b88a0839a0baaec63cf487
DanRie19 <contact@danielriera.dev>
2025-05-11 11:47:38 -0400

    Implement infinite scrolling
    - used useInfiniteQuery from react-query to implement infinite scrolling as per tanstack docs

--- mobile/components/character-list/CharacterList.tsx
+++ mobile/components/character-list/CharacterList.tsx
@@ -8,7 +8,16 @@ import Header from "@/components/header";
 import ErrorMessage from "@/components/error-message";
 
 const CharacterList: React.FC = () => {
-  const { data, isLoading, isError, refetch } = useCharacters();
+  const {
+    data,
+    isLoading,
+    isError,
+    refetch,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useCharacters();
+  const characters = data?.pages.flatMap((page) => page.data) ?? [];
 
   if (isError) {
     return <ErrorMessage onRetry={refetch} />;
@@ -23,11 +32,18 @@ const CharacterList: React.FC = () => {
             title="Morty, These Are Our People!"
           />
         }
-        data={data}
+        data={characters}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => <CharacterCard character={item} />}
         ItemSeparatorComponent={() => <HorizontalDivider />}
         showsVerticalScrollIndicator={false}
+        onEndReached={() => {
+          if (hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+          }
+        }}
+        onEndReachedThreshold={0.5}
+        ListFooterComponent={isFetchingNextPage ? <LoadingIndicator /> : null}
       />
       {isLoading && <LoadingIndicator />}
     </>
--- mobile/hooks/useCharacters.ts
+++ mobile/hooks/useCharacters.ts
@@ -1,14 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchCharacters = async () => {
-  const response = await axios.get("https://rickandmortyapi.com/api/character");
-  return response.data.results;
+const fetchCharacters = async ({ pageParam = 1 }) => {
+  const response = await axios.get(
+    `https://rickandmortyapi.com/api/character?page=${pageParam}`
+  );
+  return {
+    data: response.data.results,
+    nextPage: response.data.info.next ? pageParam + 1 : undefined,
+  };
 };
 
 export const useCharacters = () => {
-  return useQuery({
+  return useInfiniteQuery({
     queryKey: ["characters"],
     queryFn: fetchCharacters,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
   });
 };
